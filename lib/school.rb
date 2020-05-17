ecf94daf921b6c599058a57cdf0f3f1e5b21903b
Anmol Kaur <kaur.anmol26@gmail.com>
2020-05-17 07:09:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,4 +15,16 @@ def add_student(name, grade)
     if @roster.include?(grade) == false
       @roster[grade] = []
     end
+@roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
