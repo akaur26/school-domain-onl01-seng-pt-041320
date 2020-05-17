2fa6db62174dd09477e90597b53ae01cb1fbd5c3
Anmol Kaur <kaur.anmol26@gmail.com>
2020-05-17 07:11:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,13 +17,3 @@ def add_student(name, grade)
     end
     @roster[grade] << name
   end
-
-  def grade(number)
-    @roster[number]
-  end
-
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-end
